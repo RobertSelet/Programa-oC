affab8d98339346a49e2847e741438c4f6d109d3
RobertSelet <robert.cmcops@gmail.com>
2025-09-03 12:05:17 -0300

    Add files via upload
    Exercicio Estacio

---
+++ [Completo]_Super_Trunfo.c
@@ -0,0 +1,248 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+// Aula 1 (Aprendendo o basico.)
+//
+// int idade; (para numero inteiros)
+// float altura; (para números com virgulas)
+// char opcao; (usado para nomes) 
+ 
+ 
+ // Aula 2 (Entrada e saída de dados)
+ // 
+ // %d: Imprime um inteiro no formato decimal.
+ //%i: Equivalente a %d.
+ // %f: Imprime um número de ponto flutuante no formato padrão.
+ // %e: Imprime um número de ponto flutuante na notação científica.
+ // %c: Imprime um único caractere.
+ // %s: Imprime uma cadeia (string) de caracteres.
+
+
+
+  // O que vou usar no Desafio!!!!
+
+  //system("clear");
+  
+  //printf("--------------------------------------\n");
+
+int main(){
+
+
+char estado1[15], estado2[15];
+char codigo1[15], codigo2[15];
+char cidade1[15], cidade2[15];
+int popu1, popu2;
+float area1, area2;
+float pib1, pib2;
+int pontostu1, pontostu2;
+
+
+
+
+printf("[Carta 1] Digite o ESTADO da Carta:\n\n");
+scanf("%s", &estado1);
+system("clear");
+
+
+printf("--------------------------------------\n\n");
+printf("[Carta 1] Informações:\n\n");
+printf("• Estado: %s.\n\n", estado1);
+printf("--------------------------------------\n\n");
+
+
+printf("[Carta 1] Digite o Codigo da Carta:\n\n");
+scanf("%s", &codigo1);
+system("clear");
+
+
+printf("--------------------------------------\n\n");
+printf("[Carta 1] Informações:\n\n");
+printf("• Estado: %s.\n", estado1);
+printf("• Código: %s.\n\n", codigo1);
+printf("--------------------------------------\n\n");
+
+
+printf("[Carta 1] Digite a Cidade da Carta (Apenas uma Palavra):\n\n");
+scanf("%s", cidade1);
+system("clear");
+
+
+printf("--------------------------------------\n\n");
+printf("[Carta 1] Informações:\n\n");
+printf("• Estado: %s.\n", estado1);
+printf("• Código: %s.\n", codigo1);
+printf("• Cidade: %s.\n\n", cidade1);
+printf("--------------------------------------\n\n");
+
+
+printf("[Carta 1] Digite Número Populacional da Carta:\n\n");
+scanf("%d", &popu1);
+system("clear");
+
+
+printf("--------------------------------------\n\n");
+printf("[Carta 1] Informações:\n\n");
+printf("• Estado: %s.\n", estado1);
+printf("• Código: %s.\n", codigo1);
+printf("• Cidade: %s.\n", cidade1);
+printf("• População: %d.\n\n", popu1);
+printf("--------------------------------------\n\n");
+
+
+printf("[Carta 1] Digite a Área da Carta (Digite somente o Número):\n\n");
+scanf("%f", &area1);
+system("clear");
+
+
+printf("--------------------------------------\n\n");
+printf("[Carta 1] Informações:\n\n");
+printf("• Estado: %s.\n", estado1);
+printf("• Código: %s.\n", codigo1);
+printf("• Cidade: %s.\n", cidade1);
+printf("• População: %d.\n", popu1);
+printf("• Área: %f KM².\n\n", area1);
+printf("--------------------------------------\n\n");
+
+
+printf("[Carta 1] Digite o PIB da Carta (Digite somente o Número):\n\n");
+scanf("%f", &pib1);
+system("clear");
+
+
+printf("--------------------------------------\n\n");
+printf("[Carta 1] Informações:\n\n");
+printf("• Estado: %s.\n", estado1);
+printf("• Código: %s.\n", codigo1);
+printf("• Cidade: %s.\n", cidade1);
+printf("• População: %d.\n", popu1);
+printf("• Área: %f KM².\n", area1);
+printf("• PIB: %f Bilhões de Reais.\n\n", pib1);
+printf("--------------------------------------\n\n");
+
+
+printf("[Carta 1] Digite a Quantidade de Pontos Turísticos da Carta:\n\n");
+scanf("%d", &pontostu1);
+system("clear");
+
+
+printf("--------------------------------------\n\n");
+printf("[Carta 1] Informações:\n\n");
+printf("• Estado: %s.\n", estado1);
+printf("• Código: %s.\n", codigo1);
+printf("• Cidade: %s.\n", cidade1);
+printf("• População: %d.\n", popu1);
+printf("• Área: %f KM².\n", area1);
+printf("• PIB: %f Bilhões de Reais.\n", pib1);
+printf("• Pontos Turísticos: %d.\n\n", pontostu1);
+printf("--------------------------------------\n\n");
+
+
+printf("[Carta 2] Digite o ESTADO da Carta:\n\n");
+scanf("%s", &estado2);
+system("clear");
+
+
+printf("--------------------------------------\n\n");
+printf("[Carta 2] Informações:\n\n");
+printf("• Estado: %s.\n\n", estado2);
+printf("--------------------------------------\n\n");
+
+
+printf("[Carta 2] Digite o Codigo da Carta:\n\n");
+scanf("%s", &codigo2);
+system("clear");
+
+
+printf("--------------------------------------\n\n");
+printf("[Carta 2] Informações:\n\n");
+printf("• Estado: %s.\n", estado2);
+printf("• Código: %s.\n\n", codigo2);
+printf("--------------------------------------\n\n");
+
+
+printf("[Carta 2] Digite a Cidade da Carta (Apenas uma Palavra):\n\n");
+scanf("%s", cidade2);
+system("clear");
+
+
+printf("--------------------------------------\n\n");
+printf("[Carta 2] Informações:\n\n");
+printf("• Estado: %s.\n", estado2);
+printf("• Código: %s.\n", codigo2);
+printf("• Cidade: %s.\n\n", cidade2);
+printf("--------------------------------------\n\n");
+
+
+printf("[Carta 2] Digite Número Populacional da Carta:\n\n");
+scanf("%d", &popu2);
+system("clear");
+
+
+printf("--------------------------------------\n\n");
+printf("[Carta 2] Informações:\n\n");
+printf("• Estado: %s.\n", estado2);
+printf("• Código: %s.\n", codigo2);
+printf("• Cidade: %s.\n", cidade2);
+printf("• População: %d.\n\n", popu2);
+printf("--------------------------------------\n\n");
+
+
+printf("[Carta 2] Digite a Área da Carta (Digite somente o Número):\n\n");
+scanf("%f", &area2);
+system("clear");
+
+
+printf("--------------------------------------\n\n");
+printf("[Carta 2] Informações:\n\n");
+printf("• Estado: %s.\n", estado2);
+printf("• Código: %s.\n", codigo2);
+printf("• Cidade: %s.\n", cidade2);
+printf("• População: %d.\n", popu2);
+printf("• Área: %f KM².\n\n", area2);
+printf("--------------------------------------\n\n");
+
+
+printf("[Carta 2] Digite o PIB da Carta (Digite somente o Número):\n\n");
+scanf("%f", &pib2);
+system("clear");
+
+
+printf("--------------------------------------\n\n");
+printf("[Carta 2] Informações:\n\n");
+printf("• Estado: %s.\n", estado2);
+printf("• Código: %s.\n", codigo2);
+printf("• Cidade: %s.\n", cidade2);
+printf("• População: %d.\n", popu2);
+printf("• Área: %f KM².\n", area2);
+printf("• PIB: %f Bilhões de Reais.\n\n", pib2);
+printf("--------------------------------------\n\n");
+
+
+printf("[Carta 2] Digite a Quantidade de Pontos Turísticos da Carta:\n\n");
+scanf("%d", &pontostu2);
+system("clear");
+
+
+printf("--------------------------------------\n\n");
+printf("[Carta 1] Informações:\n\n");
+printf("• Estado: %s.\n", estado1);
+printf("• Código: %s.\n", codigo1);
+printf("• Cidade: %s.\n", cidade1);
+printf("• População: %d.\n", popu1);
+printf("• Área: %f KM².\n", area1);
+printf("• PIB: %f Bilhões de Reais.\n", pib1);
+printf("• Pontos Turísticos: %d.\n\n", pontostu1);
+printf("--------------------------------------\n\n");
+printf("[Carta 2] Informações:\n\n");
+printf("• Estado: %s.\n", estado2);
+printf("• Código: %s.\n", codigo2);
+printf("• Cidade: %s.\n", cidade2);
+printf("• População: %d.\n", popu2);
+printf("• Área: %f KM².\n", area2);
+printf("• PIB: %f Bilhões de Reais.\n", pib2);
+printf("• Pontos Turísticos: %d.\n\n", pontostu2);
+printf("--------------------------------------\n\n");
+
+
+return 0;
+}
